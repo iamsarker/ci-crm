eea9691f266e19d17cee99102bd7bd4077854e10
Md. Shahadat Sarker <me.sarker@gmail.com>
2026-01-31 22:25:29 +0600

    toast bug fixed in client area

--- src/views/whmazadmin/include/footer.php
+++ src/views/whmazadmin/include/footer.php
@@ -1,20 +1,20 @@
 	<script>
-		<?php $alert_success = $this->session->flashdata('alert_success'); ?>
-		<?php $this->session->set_flashdata('alert_success', NULL); ?>
-		<?php if ($alert_success) { ?>
-		toastSuccess(<?= json_encode(htmlspecialchars($alert_success, ENT_QUOTES, 'UTF-8')) ?>);
+		<?php $admin_success = $this->session->flashdata('admin_success'); ?>
+		<?php $this->session->set_flashdata('admin_success', NULL); ?>
+		<?php if ($admin_success) { ?>
+		toastSuccess(<?= json_encode(htmlspecialchars($admin_success, ENT_QUOTES, 'UTF-8')) ?>);
 		<?php } ?>
-		<?php $alert_error = $this->session->flashdata('alert_error'); ?>
-		<?php $this->session->set_flashdata('alert_error', NULL); ?>
-		<?php if ($alert_error) { ?>
-		toastError(<?= json_encode(htmlspecialchars($alert_error, ENT_QUOTES, 'UTF-8')) ?>);
+		<?php $admin_error = $this->session->flashdata('admin_error'); ?>
+		<?php $this->session->set_flashdata('admin_error', NULL); ?>
+		<?php if ($admin_error) { ?>
+		toastError(<?= json_encode(htmlspecialchars($admin_error, ENT_QUOTES, 'UTF-8')) ?>);
 		<?php } ?>
 	</script>
 
 	<footer class="footer">
 		<div>
 			<span>&copy; 2014-<?=date("Y")?> WHMAZ v1.0.0 </span>
-			<span>Maintain by <a href="https://tongbari.com/">TongBari</a></span>
+			<span>Maintain by <a href="https://whmaz.com/">WHMAZ</a></span>
 		</div>
 		<div>
 			<nav class="nav">
